6143923e0bbf0d5f9bd20545eca7bbe2105c9043
symnrtzl <symnrtzl99@gmail.com>
2023-12-02 16:10:50 +0300

    category added, response model classes updated

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,19 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/ListResponseModel';
-import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {
+export class CategoryService {
 
-  apiUrl = "https://localhost:44374/api/products/getall";
+  apiUrl = "https://localhost:44374/api/categories/getall";
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>>{
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories():Observable<ListResponseModel<Category>>{
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 
 }
