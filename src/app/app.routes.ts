c261b3a507f50f2ea47ede163095b2bf377d83a4
symnrtzl <symnrtzl99@gmail.com>
2023-12-02 22:35:59 +0300

    When the category was clicked, the products which belong to the category were listed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:ProductComponent},
+    {path:"products", component:ProductComponent},
+    {path:"products/category/:categoryId", component:ProductComponent}
+];
